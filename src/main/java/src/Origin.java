4ffbd783d9c1bc14662a8573190198deaa86bce1
1toine <antoine.lascou@epsi.fr>
2022-11-28 17:10:00 +0100

    add class Origin et Personne

--- src/main/java/src/Origin.java
+++ src/main/java/src/Origin.java
@@ -1,4 +1,14 @@
 package src;
 
 public enum Origin {
+    FRANCAIS(18),
+    ESPAGNOL(18),
+    ARGENTIN(18),
+    TUNISIEN(18),
+    AMERICAIN(21),
+    JAPONAIS(20);
+
+    int ageAdulte;
+
+    Origin(int ageAdulte) { this.ageAdulte = ageAdulte; }
 }
--- src/main/java/src/Personne.java
+++ src/main/java/src/Personne.java
@@ -1,5 +1,38 @@
 package src;
 
 public class Personne {
+    String nom;
+    Origin origin;
+    int age;
 
+    Personne() {
+        this.nom = "Laversieb";
+        this.origin = Origin.AMERICAIN;
+        this.age = 42;
+    }
+
+    Personne(String nom, Origin origin, int age) {
+        this.nom = nom;
+        this.origin = origin;
+        this.age = age;
+    }
+
+    public boolean isAdulte() {
+        if (age > origin.ageAdulte) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void afficherInformations() {
+        boolean bool = isAdulte();
+        String adult;
+        if (bool) {
+            adult = "adulte";
+        } else {
+            adult = "enfant";
+        }
+        System.out.println("Mon nom est " + nom + ". Je suis un " + origin + ". Je suis un " + adult);
+    }
 }
