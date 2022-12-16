65eec530e9e20689b85517b8d5c31b3db238f1eb
alaa.dhieb1 <alaa.dhieb1@gmail.com>
2022-12-16 22:37:25 +0100

    feat/hooks : ajout des deux scripts de hooks test

--- src/.githooks/pre-commit.sh
+++ src/.githooks/pre-commit.sh
@@ -3,17 +3,13 @@
 PWD=`pwd`
 globalEmail=`git config --global --get user.email`
 signingKey=`git config --global --get user.signingkey`
-workEmail="alaa.dhieb@epsi.fr"
+workEmail="alaa.dhieb1@gmail.com"
 
   if [[ $PWD != "*demo*" && $globalEmail != $workEmail ]];
     then
           echo "Commit email and global git config email differ"
           echo "Global commit email: "$globalEmail""
           echo "Committing email expected: $workEmail"
-          exit 1
-  elif [[ $signingKey -eq "" ]];
-    then
-          echo "No signing key found. Check global gitconfig"
           exit 1
   else
           echo ""
